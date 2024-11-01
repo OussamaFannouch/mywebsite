c629336a2b9da0a0d60541e3b7f60cf5adc4d4b3
Oussama Fannouch <oussamafannouch@gmail.com>
2024-11-01 20:51:10 +0000

    mysite

---
+++ commands.js
@@ -0,0 +1,71 @@
+aboutme = [
+  "<br>",
+"Hello, I'm Oussama Fannouch👋, a cybersecurity and intelligent network engineering student. I love participating in Capture The Flag (CTF) challenges and exploring penetration testing techniques. My passion lies in understanding and securing networks against threats.",
+"I spend my time playing CTFs and creating Python scripts, enhancing my skills and applying them in real-world scenarios. My journey has led me to the fascinating world of intelligent networks, where security meets innovation. This has inspired me to share my knowledge and experiences online.",
+"I'm focused on expanding my expertise in cybersecurity and network engineering, always seeking new challenges and opportunities to grow.",
+"<span style='color: #F5F7F8;'>Certifications:</span>",
+"<span style='color: #F5F7F8;'>- <a href='https://www.credly.com/badges/00630b53-be01-4fb7-bb4a-75b084e835eb/linked_in_profile' target='_blank'>CCNA: Switching, Routing, and Wireless Essentials</a></span>",
+"<span style='color: #F5F7F8;'>- <a href='https://www.credly.com/badges/7d00c069-462e-4192-a3ae-089fa0056b66/linked_in_profile' target='_blank'>Google Cybersecurity Certificate.</a></span>",
+"<span style='color: #F5F7F8;'>- <a href='https://www.credly.com/badges/3c7beda3-547a-4504-8b21-d55da6a983f7/linked_in_profile' target='_blank'>Fortinet Certified Fundamentals Cybersecurity</a></span>",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "On All Levels Except Physical, I am a Wolf",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'twitter        <a href="' + "https://x.com/CobraFannouch" + '" target="_blank">twitter/CobraFannouch' + '</a>',
+  'linkedin       <a href="' + "https://www.linkedin.com/in/oussamafannouch/" + '" target="_blank">linkedin/oussamafannouch' + "</a>",
+  'github         <a href="' + "https://github.com/OussamaFannouch" + '" target="_blank">github/oussamafannouch' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo su</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  '<a href="https://github.com/OussamaFannouch/SniffMaster" target="_blank">SNIFFMASTER</a> - A Simple Network Sniffer',
+  "SNIFFMASTER is a straightforward network sniffer designed by Fannouch Oussama (D1B). This tool captures and analyzes network packets, providing a command-line interface for filtering by IP address, port, HTTP requests, and TCP flags.",
+  "<br>",
+];
+
+
+help = [
+  "<br>",
+  '<span class="command">aboutme</span>        Who is Oussama Fannouch?',
+  '<span class="command">whoami</span>         Who am i?',
+  '<span class="command">sudo su</span>        Are you a wolf?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">projects</span>       View my projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           It\'s self-explanatory; do you need it spelled out?',
+  '<span class="command">email</span>          Email me only if you want to hire me :D',
+  '<span class="command">clear</span>          Clear terminal',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">Welcome to Fannouch Oussama\'s(D1B) portfolio website.</span>',
+"                  _        _							",
+"                 /\\     ,'/|     ░▒▓███████▓▒░   ░▒▓█▓▒░▒▓███████▓▒░ 	",
+"               _|  |\-'-'_/_/     ░▒▓█▓▒░░▒▓█▓▒░▒▓████▓▒░▒▓█▓▒░░▒▓█▓▒░  	",
+"          __--'/`           \     ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 	",
+"              /              \    ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░▒▓███████▓▒░ 	",
+"             /        \"o.  |o\"|     						                ",
+"             |              \/            FANNOUCH OUSSAMA			",
+"              \_          ___\       					",
+"                `--._`.   \;//    ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 	",
+"                     ;-.___,'    ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░	",
+"                    /            ░▒▓███████▓▒░   ░▒▓█▓▒░▒▓███████▓▒░  	",
+"                  ,'								",
+"               _								",
+  "<span class=\"color2\">To list the available commands, use</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
